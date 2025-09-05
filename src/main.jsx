a2a9a68c33694e3e85b050665492746948f442fd
grantdozier <gdozier110@gmail.com>
2025-09-05 15:08:11 -0500

    fixed base directory for serving on github pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const router = createBrowserRouter([{ path: "/", element: <App /> }], {
+  basename: "/moods"
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
